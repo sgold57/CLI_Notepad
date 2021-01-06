6bf1e65be1e800c362201974d55dbf64b5352da8
Sam Gold <samg915757@gmail.com>
2021-01-06 16:51:12 -0700

    completed create function

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_212716) do
+ActiveRecord::Schema.define(version: 2021_01_06_231836) do
 
   create_table "notes", force: :cascade do |t|
     t.text "description"
-    t.string "user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- lib/cli.rb
+++ lib/cli.rb
@@ -35,16 +35,64 @@ class Cli
     end
 
     def no_account
-      puts "good refactoring"
+      new_account = prompt.yes?("Would you like to create an account?")
+      if new_account
+        system("clear")
+        create_new_account
+      else
+        puts "Kick rocks"
+      end
+    end
+
+    def create_new_account
+      puts "Please input your desired username: "
+      desired_username = gets.chomp
+      verify = prompt.yes?("Are you sure you want #{desired_username} to be your username?")
+      if verify
+        User.create(username: desired_username)
+        binding.pry
+      end
     end
 
     def user_verification(input)
-      binding.pry
       if User.find_by(username: input)
-        puts "Welcome back"
+        current_user = User.find_by(username: input)
+        puts "Welcome back #{current_user.username}."
+        main_menu(current_user)
       else
-        puts "That username does not compute"
+        puts "That username does not compute."
+        account_login
       end
+    end
+
+    def main_menu(current_user)
+      binding.pry
+      options = ["Create new note", "Read all previous notes", "Update an existing note", "Delete note"]
+      choice = prompt.select("What you you like to do today?", options)
+      if choice == "Create new note"
+        create_note(current_user)
+      end
+    end
+
+    def create_note(current_user)
+      system("clear")
+      puts("Write your note, hit enter when finished")
+      new_note = gets.chomp
+      verify = prompt.yes?("Would you like to save this note?")
+        if verify
+          Note.create(description: new_note, user: current_user)
+          anything_else = prompt.yes?("Would you like to do anything else?")
+          if anything_else
+            system("clear")
+            main_menu(current_user)
+          else
+            system("clear")
+            exit
+          end
+        else
+          system("clear")
+          create_note(current_user)
+        end
     end
 
 end
